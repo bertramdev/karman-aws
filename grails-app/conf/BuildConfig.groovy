60c794efa04edff8890295fff63786905d096f44
David Estes <davydotcom@gmail.com>
2014-02-20 20:10:45 -0500

    cleaning up for plugin approval

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,21 +1,15 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = 'target'
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+    inherits 'global'
+    log "warn"
+    legacyResolve false
     repositories {
         grailsCentral()
         grailsPlugins()
         mavenCentral()
-        
     }
+
     dependencies {
         compile 'net.java.dev.jets3t:jets3t:0.9.0'
         build 'org.apache.httpcomponents:httpcore:4.2'
@@ -25,12 +19,12 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
+        runtime ":karman:0.1.0"
         build(":tomcat:$grailsVersion",
-              ":release:2.2.1",
-              ":rest-client-builder:1.0.3") {
+              ":release:2.2.1") {
             export = false
         }
     }
 }
 
-grails.plugin.location."karman" = "../karman"
+// grails.plugin.location."karman" = "../karman"
