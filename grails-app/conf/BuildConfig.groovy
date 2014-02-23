aba48fa0465dd7d957e89c2ff3a6cedc61faf1c3
Benoit Hediard <ben@agorapulse.com>
2014-02-23 16:15:05 +0100

    Grails upgraded to 2.3.6

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,8 +1,9 @@
 grails.project.work.dir = 'target'
 
+grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
     inherits 'global'
-    log "warn"
+    log 'warn'
     legacyResolve false
     repositories {
         grailsCentral()
@@ -19,12 +20,14 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        // runtime ":karman:0.1.1"
-        build(":tomcat:$grailsVersion",
-              ":release:2.2.1") {
+        runtime ':karman:0.1.1'
+        build ':tomcat:7.0.50.1'
+        runtime ':hibernate:3.6.10.8'
+
+        build(':release:3.0.1') {
+
             export = false
         }
     }
 }
-
-grails.plugin.location."karman" = "../karman"
+//grails.plugin.location."karman" = "../karman"
