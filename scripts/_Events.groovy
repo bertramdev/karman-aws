9d7a25be6ac4977bd2ebc7a4bafce7dd507256c9
David Estes <davydotcom@gmail.com>
2014-12-10 13:23:24 -0800

    removed unnecessary code

--- scripts/_Events.groovy
+++
@@ -1,9 +0,0 @@
-
-eventKarmanConfig = { configHolder ->
-	def s3StorageProvider = classLoader.loadClass('com.bertramlabs.plugins.karman.aws.S3StorageProvider')
-	
-	configHolder.providerTypes += [
-		s3: s3StorageProvider,
-		aws: s3StorageProvider
-	]
-}
