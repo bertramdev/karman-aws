05a01acb835634f38bcac5c12bdd066e1d69c5e9
David Estes <davydotcom@gmail.com>
2015-12-02 20:05:09 -0500

    well now, what have we here

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -18,17 +18,17 @@ grails.project.dependency.resolution = {
         // runtime 'org.apache.httpcomponents:httpcore:4.2'
         // runtime 'org.apache.httpcomponents:httpclient:4.2'
         runtime 'com.amazonaws:aws-java-sdk:1.10.33'
-        compile('com.bertramlabs.plugins:karman-aws-groovy:0.8.5') {
+        compile('com.bertramlabs.plugins:karman-aws:0.9.8') {
             excludes 'karman-core'
         }
     }
 
     plugins {
         build ':tomcat:7.0.52.1'
-        build(':release:3.0.1') {
+        build(':release:3.1.2') {
             export = false
         }
-        runtime ':karman:0.8.3'
+        runtime ':karman:0.9.8'
 
         test ':code-coverage:1.2.7'
     }
